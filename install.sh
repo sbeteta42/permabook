c79efb816ab4d4542b9f01c9d053f0c2ee26e1fa
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-01-21 15:24:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 # Mise à jour de l'Os.. Please wait...
 echo "Mise à jour de l'Os.. Please wait..."
 apt update && apt full-upgrade -y
+clear
 
 # Installation des dpendances fonctionnelles
 echo " Installation des dpendances fonctionnelles..."
 apt install apache2 mariadb-server mariadb-client php libapache2-mod-php php-cli php-mysql php-zip php-curl php-xml php-gd php-intl php-mbstring php-imagick -y
+clear
 
 # On stoppe le service Apache2
 echo "On stoppe le service Apache2" 
@@ -13,6 +15,7 @@ service apache2 stop
 # Download de PERMABOOK
 echo "Download de PERMABOOK..."
 wget https://github.com/hivecloud-iris/permabook/raw/main/sitePermabook.zip -O /tmp/sitePermabook.zip
+clear
 
 #INSTALLATION DE PERMABOOK
 echo "Installation de PERMABOOK... 
