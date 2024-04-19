f842ac60a1d72d26b8716f64335ef18e0cb60481
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-04-19 11:00:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,29 @@
 # Mise à jour de l'Os.. Please wait...
 echo "Mise à jour de l'Os.. Please wait..."
 apt update && apt full-upgrade -y
+sleep 5
 clear
 
 # Installation des dpendances fonctionnelles
 echo "Installation des dpendances fonctionnelles..."
+sleep 5
 apt install apache2 mariadb-server mariadb-client php libapache2-mod-php php-cli php-mysql php-zip php-curl php-xml php-gd php-intl php-mbstring php-imagick -y
 clear
 
 # On stoppe le service Apache2
 echo "On stoppe le service Apache2" 
+sleep 5
 service apache2 stop
 
 # Download de PERMABOOK
 echo "Download de PERMABOOK..."
+sleep 5
 wget http://download1.beteta.org:9658/share/FGc-TwVA-k9P6P8E/sitePermabook.zip -O /tmp/sitePermabook.zip
 clear
 
 #INSTALLATION DE PERMABOOK
-echo "Installation de PERMABOOK..." 
+echo "Installation de PERMABOOK..."
+sleep5
 mkdir -p /var/www/html/certa/permabook
 unzip -d /tmp/ /tmp/sitePermabook.zip
 unzip -d /var/www/html/certa/permabook/ /tmp/sitePermabook/permabook.zip
