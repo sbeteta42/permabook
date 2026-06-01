b1eb88fa6ecd87c206e6ac845b8b73bf1532ef2c
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2026-06-01 14:35:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ service apache2 stop
 # Download de PERMABOOK
 echo "[4/5] Download de PERMABOOK..."
 sleep 5
-wget https://mega.nz/file/SgAzWKYK#xvxehYh0aVt6ATknm3fOPOG_wLngFiWZ-VybLye7oGk -O /tmp/sitePermabook.zip
+mega-get 'https://mega.nz/file/SgAzWKYK#xvxehYh0aVt6ATknm3fOPOG_wLngFiWZ-VybLye7oGk' /tmp/sitePermabook.zip
 clear
 
 #INSTALLATION DE PERMABOOK
